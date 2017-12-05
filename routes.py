0ea84ae49d65a3029487547f2492e656eb6c3a8f
Ahmad Jarir <173329Y@mymail.nyp.edu.sg>
2017-12-05 15:12:21 +0800

    added email function, but doesn't seem to work

--- routes.py
+++ routes.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message
 app = Flask(__name__)
 
+app.config.update(
+	DEBUG= True,
+	MAIL_SERVER='smtp.gmail.com',
+	MAIL_PORT= 465,
+	MAIL_USE_SSL= True,
+	MAIL_USERNAME = 'jonsnow3050@gmail.com',
+	MAIL_PASSWORD = 'jonsnowisdead',
+    MAIL_DEFAULT_SENDER = 'jonsnow3050@gmail.com'
+)
 
-@app.route('/')
+mail = Mail(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        msg = Message(subject="Feedback",
+                      recipients=['jonsnow3050@gmail.com'])
+        msg.html = 'From: {} ({}) <br> <br> Subject: {} <br> <br> Message: {}'.format(request.form['name'],
+                                                                                      request.form['email'],
+                                                                                      request.form['subject'],
+                                                                                      request.form['message'])
+        mail.send(msg)
+        return render_template('index.html')
+
     return render_template('index.html')
+
+
+
 
 if __name__ == '__main__':
     app.run()
